e5ce563bfc97f24ad5b9d2f97d751880a42aa690
prongbang <prongbang@gmail.com>
2025-04-24 17:58:59 +0700

    config: swift package manager

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ let package = Package(
     products: [
         .library(
             name: "MaterialComponents",
-            targets: ["MaterialComponentsWrapper"]
-        )
+            targets: ["MaterialComponents"]
+        ),
     ],
     dependencies: [
         .package(url: "https://github.com/prongbang/material-internationalization-ios.git", branch: "main"),
@@ -17,15 +17,6 @@ let package = Package(
             name: "MaterialComponents",
             url: "https://github.com/prongbang/material-components-ios-spm/releases/download/124.2.0/MaterialComponents.xcframework.zip",
             checksum: "d7b83a1bc307dbddb44ecdd0c031752fa26a872e51232fa59ecfc02068b3bc2a"
-        ),
-        .target(
-            name: "MaterialComponentsWrapper",
-            dependencies: [
-                "MaterialComponents",
-                .product(name: "MDFInternationalization", package: "material-internationalization-ios"),
-                .product(name: "MDFTextAccessibility", package: "material-text-accessibility-ios"),
-            ],
-            path: "Sources/MaterialComponentsWrapper"
         )
     ]
 )
