c3e99fcc4630daaa9792b1a2fbe6eb7b25a3e292
prongbang <prongbang@gmail.com>
2025-04-29 10:34:24 +0700

    config: update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version:5.7
 import PackageDescription
+
 let package = Package(
     name: "MaterialComponents",
     products: [
@@ -18,17 +19,22 @@ let package = Package(
             url: "https://github.com/prongbang/material-components-ios-spm/releases/download/124.2.0/MaterialComponents.xcframework.zip",
             checksum: "d7b83a1bc307dbddb44ecdd0c031752fa26a872e51232fa59ecfc02068b3bc2a"
         ),
+        // This just forwards the dependency products
         .target(
             name: "MDFInternationalizationPackage",
             dependencies: [
                 .product(name: "MDFInternationalization", package: "material-internationalization-ios")
-            ]
+            ],
+            path: "Sources",
+            sources: ["Dummy.swift"]
         ),
         .target(
             name: "MDFTextAccessibilityPackage",
             dependencies: [
                 .product(name: "MDFTextAccessibility", package: "material-text-accessibility-ios")
-            ]
+            ],
+            path: "Sources",
+            sources: ["Dummy.swift"]
         )
     ]
 )
